220eb45c31cbf228e160d3b051fde6b9ebb31e62
VladislavGonchar <vladgonchar103@gmail.com>
2025-04-05 12:01:04 +0300

    Кривий дизайн в студент форм з екзаменами

--- StudentApp/StudentForm.cs
+++ StudentApp/StudentForm.cs
@@ -1,5 +1,4 @@
-﻿// StudentForm (with validation and dialog prompt)
-using System;
+﻿using System;
 using System.Windows.Forms;
 using StudentLibrary.Models;
 
@@ -9,11 +8,16 @@ namespace StudentApp
     {
         public Student Student { get; private set; }
 
+        private ListBox listBoxExams;
+        private Button buttonAddExam;
+        private Button buttonEditExam;
+        private Button buttonDeleteExam;
+
         public StudentForm(Student student = null)
         {
             InitializeComponent();
 
-            // Оновлюємо список варіантів у випадаючому меню (тепер українською)
+            // Заповнюємо рівні освіти
             comboBoxLevel.Items.Clear();
             comboBoxLevel.Items.AddRange(new[] { "Бакалавр", "Спеціаліст", "Магістр" });
 
@@ -29,6 +33,9 @@ namespace StudentApp
             {
                 Student = new Student();
             }
+
+            InitializeExamControls(); // Додаємо управління для іспитів
+            UpdateExamList(); // Оновлюємо список іспитів
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
@@ -55,6 +62,95 @@ namespace StudentApp
 
             DialogResult = DialogResult.OK;
             Close();
+        }
+
+        // --------------------- Елементи управління іспитами ---------------------
+
+        private void InitializeExamControls()
+        {
+            // ListBox для іспитів
+            listBoxExams = new ListBox
+            {
+                Top = 200,
+                Left = 20,
+                Width = 300,
+                Height = 100
+            };
+            Controls.Add(listBoxExams);
+
+            // Кнопка додати
+            buttonAddExam = new Button
+            {
+                Text = "Додати іспит",
+                Top = listBoxExams.Bottom + 10,
+                Left = listBoxExams.Left,
+                Width = 90
+            };
+            buttonAddExam.Click += buttonAddExam_Click;
+            Controls.Add(buttonAddExam);
+
+            // Кнопка редагувати
+            buttonEditExam = new Button
+            {
+                Text = "Редагувати",
+                Top = listBoxExams.Bottom + 10,
+                Left = buttonAddExam.Right + 10,
+                Width = 90
+            };
+            buttonEditExam.Click += buttonEditExam_Click;
+            Controls.Add(buttonEditExam);
+
+            // Кнопка видалити
+            buttonDeleteExam = new Button
+            {
+                Text = "Видалити",
+                Top = listBoxExams.Bottom + 10,
+                Left = buttonEditExam.Right + 10,
+                Width = 90
+            };
+            buttonDeleteExam.Click += buttonDeleteExam_Click;
+            Controls.Add(buttonDeleteExam);
+        }
+
+        private void UpdateExamList()
+        {
+            listBoxExams.DataSource = null;
+            listBoxExams.DataSource = Student.Exams;
+        }
+
+        private void buttonAddExam_Click(object sender, EventArgs e)
+        {
+            ExamForm examForm = new ExamForm();
+            if (examForm.ShowDialog() == DialogResult.OK)
+            {
+                Student.Exams.Add(examForm.Exam);
+                UpdateExamList();
+            }
+        }
+
+        private void buttonEditExam_Click(object sender, EventArgs e)
+        {
+            if (listBoxExams.SelectedItem is Exam selectedExam)
+            {
+                ExamForm examForm = new ExamForm(selectedExam);
+                if (examForm.ShowDialog() == DialogResult.OK)
+                {
+                    UpdateExamList(); // оновлюємо список
+                }
+            }
+        }
+
+        private void buttonDeleteExam_Click(object sender, EventArgs e)
+        {
+            if (listBoxExams.SelectedItem is Exam selectedExam)
+            {
+                var result = MessageBox.Show("Ви впевнені, що хочете видалити цей іспит?", "Підтвердження", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    Student.Exams.Remove(selectedExam);
+                    UpdateExamList();
+                }
+            }
         }
     }
 }
